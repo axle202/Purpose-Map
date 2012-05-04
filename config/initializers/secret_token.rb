f47ac7f6e6d065a6f7150aaf099eb19d2c340fde
Axle <axle202@gmail.com>
2012-05-04 13:30:38 -0400

    Initial Pmap code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PurposeMap::Application.config.secret_token = '83fb2c510c37c69a9d8f1248d2636acce10f8f72e59bf2806e5a44ea24d316f5f8fb71ad7456ebf57b9e655f00856a1c9588863fddffc5eedc5fddd54378e72c'
