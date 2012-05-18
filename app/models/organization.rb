771455a41c4ab3ebe3cee0b4c41ba5afee568827
Paul Ostazeski <paul@intridea.com>
2012-05-18 11:58:48 -0400

    Added geocoder to handle organizations' locations.

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,15 @@
 class Organization < ActiveRecord::Base
-  attr_accessible :NTEE, :Name, :ZipCode
+  attr_accessible :ntee, :name, :zip, :address, :latitude, :longitude
+  geocoded_by :full_address
+  after_validation :geocode, :if => :full_address_changed?
+  validates_presence_of :name, :address
+
+  def full_address
+    [address, zip].join(', ')
+  end
+
+  def full_address_changed?
+    address_changed? || zip_changed?
+  end
+
 end
